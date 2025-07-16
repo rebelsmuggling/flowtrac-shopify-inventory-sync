770d0f42e392645793008f22ab2f99b36d8dbed7
rebelsmuggling <James@rebelsmuggling.com>
2025-07-16 15:21:59 -0700

    Fix API routes: move to correct app router location and fix import paths

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mfz4Owdir9qf1gHpDL9Yhm+gI1prVESzwuAqioNOVEY=\"\n}"
